817bce01ae9c2ac4fb9e359bef9c6ae31f6172da
Eduin Hernandez <eduin_hernandez15@hotmail.com>
2022-08-16 10:26:00 +0800

    Update autoencoder.py
    Update

--- autoencoder.py
+++ autoencoder.py
@@ -19,46 +19,49 @@ from scipy.stats import gennorm, norm, laplace
 ### note on 8/11 try the encoder for 3 layers can convergence but more than 3 layer will convergence.
 
 # parameter for Network
-epochs = 100
-lr =0.0001
+epochs = 1000
+lr = 0.01
 
 # node for autoencoder C
-node1_C = 1000
-node2_C = 500
+batch_size = 100
+node0_C = 1000
+node1_C = 500
+node2_C = 200
 node3_C = 100
 
 # construct encoder network train compared with input & output with the MMSE without bias
 class Network_C(tf.keras.Model):
     def __init__(self):
         super().__init__()
-        self.dense1= tf.keras.layers.Dense(units = node1_C,
-                                            kernel_initializer=tf.keras.initializers.RandomNormal(mean=0, stddev=1),
-                                            use_bias=False,
-                                            )
-        self.dense2 = tf.keras.layers.Dense(units = node2_C,
-                                            kernel_initializer=tf.keras.initializers.RandomNormal(mean=0, stddev=1),
-                                            use_bias=False,
-                                            )
-        self.dense3 = tf.keras.layers.Dense(units = node3_C,
-                                            kernel_initializer=tf.keras.initializers.RandomNormal(mean=0, stddev=1),
-                                            use_bias=False,
-                                            )
+        
+        self.encoder = tf.keras.Sequential([
+          tf.keras.layers.Dense(units = node1_C, kernel_initializer=tf.keras.initializers.RandomNormal(mean=0, stddev=1),
+                       use_bias=False, activation='sigmoid'),
+          tf.keras.layers.Dense(units = node2_C, kernel_initializer=tf.keras.initializers.RandomNormal(mean=0, stddev=1),
+                       use_bias=False, activation='sigmoid'),
+           tf.keras.layers.Dense(units = node3_C, kernel_initializer=tf.keras.initializers.RandomNormal(mean=0, stddev=1),
+                        use_bias=False),
+        ])
+        
+        self.decoder = tf.keras.Sequential([
+            tf.keras.layers.Dense(units = node2_C, kernel_initializer=tf.keras.initializers.RandomNormal(mean=0, stddev=1),
+                          use_bias=False, activation='sigmoid'),
+            tf.keras.layers.Dense(units = node1_C, kernel_initializer=tf.keras.initializers.RandomNormal(mean=0, stddev=1),
+                         use_bias=False, activation='sigmoid'),
+            tf.keras.layers.Dense(units = node0_C, kernel_initializer=tf.keras.initializers.RandomNormal(mean=0, stddev=1),
+                         use_bias=False),
+        ])
 
-        self.dense4 = tf.keras.layers.Dense(units = node2_C,
-                                            kernel_initializer=tf.keras.initializers.RandomNormal(mean=0, stddev=1),
-                                            use_bias=False,
-                                            )
-        self.dense5 = tf.keras.layers.Dense(units = node1_C,
-                                            kernel_initializer=tf.keras.initializers.RandomNormal(mean=0, stddev=1),
-                                            use_bias=False,
-                                            )
     def call(self, input):
-        hidden1 = self.dense1(input)
-        hidden2 = self.dense2(hidden1)
-        hidden3 = self.dense3(hidden2)
-        hidden4 = self.dense4(hidden3)
-        output = self.dense5(hidden4)
-        return output
+        encoded = self.encoder(input)
+        decoded = self.decoder(encoded)
+        return decoded
+    
+    def encode(self, input):
+        return self.encoder(input)
+    
+    def decode(self, encoded):
+        return self.decoder(encoded)
         
 ## tool
 # change tf to numpy    
@@ -69,13 +72,15 @@ def change_type(grads):
 
 model = Network_C()
 optimizer = tf.keras.optimizers.SGD(learning_rate=lr) 
-X = np.random.normal(0, 1, size=(1000, 1)).astype(np.float32)  # input 
+
 loss_fun = tf.keras.losses.MeanSquaredError() # loss
 losses = []
 empty = []
 
 # train 
 for epoch in range(epochs):
+    'The input needs to be changing each epoch'
+    X = np.random.normal(0, 1, size=(batch_size, node0_C)).astype(np.float32)  # input 
     # record the gradient and updated the weight by the MMSE
     with tf.GradientTape() as tape: # record the forward prop
         y_pred = model(X)
@@ -87,19 +92,16 @@ for epoch in range(epochs):
     optimizer.apply_gradients(grads_and_vars=zip(grads, model.variables))
     
     # if epoch % 25 == 0:
-    #    new = change_type(grads)
-    #    # sort in an array
-    #    d = new.flatten()
-    #    empty.append(d[1])
-       
+    #     new = change_type(grads)
+    #     # sort in an array
+    #     d = new.flatten()
+    #     empty.append(d[1])
+
+plt.close('all')
+plt.figure()
 plt.plot(losses,color='red', label='loss')
 plt.xlabel('training epochs')
 plt.ylabel('training loss')
 plt.legend()
 plt.grid()
 plt.show()
-
-
-
-
-
